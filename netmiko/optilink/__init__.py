42ef358e5547214721c5fe6a5b1b84bebb8f2df5
IvaAMarinova <ivamarinova06@gmail.com>
2025-07-09 14:39:41 +0300

    Added basic 7449 Optilink implementation

--- netmiko/optilink/__init__.py
+++ netmiko/optilink/__init__.py
@@ -1,11 +1,13 @@
 from netmiko.optilink.op_golt_944 import OptilinkGOLT944Telnet
 from netmiko.optilink.op_eolt_11444 import OptilinkEOLT11444Telnet
 from netmiko.optilink.op_eolt_9702 import OptilinkEOLT9702Telnet
+from netmiko.optilink.op_eolt_7440 import OptilinkEOLT7440Telnet
 from netmiko.optilink.op_golt_924 import OptilinkGOLT924Telnet
 
 __all__ = [
     "OptilinkGOLT944Telnet",
     "OptilinkEOLT11444Telnet",
     "OptilinkEOLT9702Telnet",
+    "OptilinkEOLT7440Telnet",
     "OptilinkGOLT924Telnet",
 ]
